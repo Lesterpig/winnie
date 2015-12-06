166ce5406a9518dc39a37e9d8df2bc1b1b289f29
Quentin Dufour <quentin@dufour.tk>
2015-12-06 23:01:12 +0100

    [test] Add dijkstra test for walls

--- Winnie/Test/DijkstraTests.cs
+++ Winnie/Test/DijkstraTests.cs
@@ -19,6 +19,18 @@ namespace Test
 			Dijkstra d = new Dijkstra (data, 3, 3, new Point (0, 0));
 			Assert.AreEqual (3.0, d.getDistance (new Point (0, 2)));
 		}
+
+		[Test()]
+		public void GetDistanceWallTest()
+		{
+			double[] data = {
+				0.5, 0.5, 0.5,
+				9.0, 9.0, -1,
+				0.5, 0.5, 0.5,
+			};
+			Dijkstra d = new Dijkstra (data, 3, 3, new Point (2, 0));
+			Assert.AreEqual (10.5, d.getDistance (new Point (2, 2)));
+		}
 	}
 }
 
