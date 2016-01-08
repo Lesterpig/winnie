6eaf6f6c3c290ec1d1a11b4a59470e1d72cb4385
Quentin Dufour <quentin@dufour.tk>
2016-01-08 14:59:13 +0100

    Set to standard game type

--- Winnie/LauncherCLI/Program.cs
+++ Winnie/LauncherCLI/Program.cs
@@ -14,7 +14,7 @@ namespace Linux
 			Console.WriteLine ("Launch MonoGame !");
 			var p1 = new Player("Player A", Human.Instance);
 			var p2 = new Player("Player B", Elf.Instance);
-			var GameModel = GameBuilder.New<DemoGameType, PerlinMap>(p1, p2, true, seed);
+			var GameModel = GameBuilder.New<StandardGameType, PerlinMap>(p1, p2, true, seed);
 			GameCreator.New (GameModel, seed);
 		}
 	}
